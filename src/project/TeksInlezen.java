163c46ca39abec1149ea12ad5648cc7262a16039
bram <bram@bram-PC.wireless.hhs.nl>
2015-05-28 18:39:04 +0200

    teksInlezen

---
+++ src/project/TeksInlezen.java
@@ -0,0 +1,33 @@
+
+package project;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TeksInlezen {
+    public static void main(String[] args) throws IOException {
+        String path = "C:/Users/bram/Documents/NetBeansProjects/Doolhof-D/Doolhof/Doolhof1.txt";
+        BufferedReader d1 = new BufferedReader(new FileReader(path));
+        String line;
+        while((line = d1.readLine()) != null){
+            System.out.println(line);
+        }
+        d1.close();
+        
+        path = "C:/Users/bram/Documents/NetBeansProjects/Doolhof-D/Doolhof/Doolhof2.txt";
+        BufferedReader d2 = new BufferedReader(new FileReader(path));
+        while((line = d2.readLine()) != null){
+            System.out.println(line);
+        }
+        d2.close();
+        
+        path = "C:/Users/bram/Documents/NetBeansProjects/Doolhof-D/Doolhof/Doolhof3.txt";
+        BufferedReader d3 = new BufferedReader(new FileReader(path));
+        while((line = d3.readLine()) != null){
+            System.out.println(line);
+        }
+        d3.close();
+    }
+    
+}
