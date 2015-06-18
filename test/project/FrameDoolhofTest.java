15617a1817b27f2206396b5a86b80551149f7b5f
brams_000 <brams_000@192.168.1.112>
2015-06-18 17:14:46 +0200

    testestststsetst

--- test/project/FrameDoolhofTest.java
+++ test/project/FrameDoolhofTest.java
@@ -6,6 +6,7 @@ package project;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -40,68 +41,86 @@ public class FrameDoolhofTest {
      */
     @Test
     public void testCanMoveNorth() throws IOException {
-        System.out.println("canMove");
-        String direction = "Omhoog";
         FrameDoolhof instance = new FrameDoolhof();
         instance.opbouw(1);
         instance.startButton.doClick();
-        boolean expResult = false;
-        boolean result = instance.canMove(direction, instance.S.getX(), instance.S.getY());
+        KeyEvent key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_UP);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        int expResult = 1;
+        int result = instance.S.getY();
         assertEquals(expResult, result);
     }
 
     @Test
     public void testCanMoveEast() throws IOException {
-        System.out.println("canMove");
-        String direction = "Rechts";
         FrameDoolhof instance = new FrameDoolhof();
         instance.opbouw(1);
         instance.startButton.doClick();
-        boolean expResult = false;
-        boolean result = instance.canMove(direction, instance.S.getX(), instance.S.getY());
+        KeyEvent key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        int expResult = 1;
+        int result = instance.S.getX();
         assertEquals(expResult, result);
     }
 
     @Test
-    public void testCanMoveSouth() throws IOException {
-        System.out.println("canMove");
-        String direction = "Omlaag";
+    public void testCanMoveSouth() throws IOException, AWTException {
         FrameDoolhof instance = new FrameDoolhof();
         instance.opbouw(1);
         instance.startButton.doClick();
-        boolean expResult = true;
-        boolean result = instance.canMove(direction, instance.S.getX(), instance.S.getY());
+        KeyEvent key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_DOWN);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        int expResult = 2;
+        int result = instance.S.getY();
         assertEquals(expResult, result);
     }
 
     @Test
-    public void testCanMoveWest() throws IOException {
-        System.out.println("canMove");
-        String direction = "Links";
+    public void testCanMoveWest() throws IOException, AWTException {
         FrameDoolhof instance = new FrameDoolhof();
         instance.opbouw(1);
         instance.startButton.doClick();
-        boolean expResult = false;
-        boolean result = instance.canMove(direction, instance.S.getX(), instance.S.getY());
+        KeyEvent key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        int expResult = 1;
+        int result = instance.S.getX();
         assertEquals(expResult, result);
     }
 
     @Test
     public void schieten() throws IOException, AWTException {
-        System.out.println("Rechts");
-        String direction = "Rechts";
         FrameDoolhof instance = new FrameDoolhof();
         instance.opbouw(1);
         instance.startButton.doClick();
         instance.S.setP(4, 3);
         instance.S.setDirection('E');
-        Robot robot = new Robot();
-        robot.keyPress(39);
-        robot.keyRelease(39);
-        robot.keyPress(32);
-        robot.keyRelease(32);
-        boolean expResult = true;
-        boolean result = instance.Doolhof[6][3].loopbaar;
+        KeyEvent key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_SPACE);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        key = new KeyEvent(instance.frame, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT);
+        instance.frame.getKeyListeners()[0].keyPressed(key);
+        int expResult = 6;
+        int result = instance.S.getX();
         assertEquals(expResult, result);
+    }
+
+    @Test
+    public void lopen() throws IOException {
+        FrameDoolhof instance = new FrameDoolhof();
+        instance.opbouw(1);
+        instance.startButton.doClick();
+        instance.Dh.setStappen(1);
+        boolean een;
+        boolean twee;
+        boolean drie;
+        boolean totaal;
+        if (instance.canMove("Omlaag", 1, 1)) {
+            een = true;
+        }
+        instance.Dh.setStappen(0);
+        if(!instance.canMove("Omlaag",1,1)){
+            twee = true;
+        }
     }
 }
